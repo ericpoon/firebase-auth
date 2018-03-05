62f86f2da7a812578463ec6500c9288fd9f48417
PAN Zhiqi <ericpoon510@gmail.com>
2018-03-05 18:01:06 +0800

    Able to sign up; Able to invoke Google Cloud Functions

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-efa31.cloudfunctions.net';
 
 class SignUpForm extends Component {
+  state = { email: '' };
+
+  onSubmitPress = async () => { // use async/await (just syntactic sugar, doesn't change how JS runs/works)
+    const { email } = this.state;
+
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { email });
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { email });
+    } catch (err) {
+      console.log(err.message);
+      console.log(err.response.data);
+    }
+  };
+
   render() {
     return (
       <View>
-        <FormLabel>Enter Email Address</FormLabel>
-        <FormInput />
-        <Button title={'Submit'} />
+        <View style={{ marginBottom: 15 }}>
+          <FormLabel>Enter Email Address</FormLabel>
+          <FormInput
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </View>
+        <Button title={'Submit'} onPress={this.onSubmitPress} />
       </View>
     );
   }
